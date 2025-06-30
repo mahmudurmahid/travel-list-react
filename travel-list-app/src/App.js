2e8f7ae9659ecb71b9eea9f00edfa480a350d493
mahmudurmahid <m.mahid@icloud.com>
2025-06-30 18:04:54 +0100

    outlined all components needed for app

--- travel-list-app/src/App.js
+++ travel-list-app/src/App.js
@@ -1,1 +1,18 @@
-export default function App() {}
+export default function App() {
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {}
+
+function Form() {}
+
+function PackingList() {}
+
+function Stats() {}
