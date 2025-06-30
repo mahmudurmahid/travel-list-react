b9fd5dc3f0c4a471e55a3f5375ff9fdbde7fc9f0
mahmudurmahid <m.mahid@icloud.com>
2025-06-30 20:00:03 +0100

    strikethrough line on any added items

--- travel-list-app/src/App.js
+++ travel-list-app/src/App.js
@@ -42,7 +42,7 @@ function PackingList() {
 function Item({ item }) {
   return (
     <li>
-      <span>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
       <button>❌</button>
