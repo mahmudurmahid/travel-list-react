e2d44b26ab0f1b9b26e7b3f5ebd3d55324e6a29c
mahmudurmahid <m.mahid@icloud.com>
2025-06-30 20:07:22 +0100

    can add number of items from 1-20 on form

--- travel-list-app/src/App.js
+++ travel-list-app/src/App.js
@@ -21,9 +21,16 @@ function Logo() {
 
 function Form() {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>What do you need for your trip? 🧳</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+    </form>
   );
 }
 
